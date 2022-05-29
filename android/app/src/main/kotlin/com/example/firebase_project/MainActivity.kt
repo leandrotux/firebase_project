b650cf8b3bd2a7be3541955a5feb9b78516bd7c5
Leandro Dourado <leandrotux@hotmail.com>
2022-05-29 20:33:17 -0300

    initial project firebase

---
+++ android/app/src/main/kotlin/com/example/firebase_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sertaodev.firebase_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  db
+      .collection("Usuarios")
+      .doc("001")
+      .set({"Leandro": "500", "Vanessa": "150"});
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
